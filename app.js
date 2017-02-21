65355f7e3fd20c0840f894bd8a31a74ae145b89f
Thomas Daalder <thomasdaalder@gmail.com>
2017-02-21 17:42:01 +0100

    Finished assignment. Now going to make it pretty

---
+++ app.js
@@ -0,0 +1,52 @@
+const 	express = require('express'),
+		fs = require('fs'),
+		app = express(),
+		bodyParser = require('body-parser'),
+		pg = require('pg'),
+		db = require(__dirname + '/models/db.js')
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.use(express.static('./', {
+}));
+
+// app.use(bodyParser.json('/users.json')); // support json encoded bodies
+app.use(bodyParser.urlencoded({	extended: true })); // support encoded bodies
+// app.use(express.static('static'))
+
+app.get('/', (request, response) => {
+	response.render('index');
+
+});
+
+app.get('/messages', function(req, res) {
+	  db.Messages.findAll()
+    .then((allMessages) => {
+        res.render('messages', {names: allMessages});
+    })
+});
+
+app.post('/messages', function(req, res){
+	console.log(req.body)
+	db.Messages.create({
+		title: req.body.title,
+		body: req.body.body
+	})
+	.then(()=>{
+		res.redirect('/messages');
+	})
+});
+
+// app.get('/messages', function(req, res){
+//     db.Messages.findAll()
+//     .then((allMessages) => {
+//         res.render('messages', {names: allMessages});
+//     })    
+// })
+
+
+app.listen(3000, () => {
+	console.log('Server has started.');
+});
---
+++ models/db.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const db = new Sequelize( 'bulletinboard', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+	dialect: 'postgres'
+});
+ 
+const Messages = db.define('messages', {
+    title: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+    body: {
+		type: Sequelize.STRING,
+		allowNull: false,
+	},
+});
+
+db.sync()
+
+.catch( (error) => console.log(error) );
+
+module.exports = {
+    db: db, 
+    Messages: Messages
+}
